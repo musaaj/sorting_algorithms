26896bd2f898ffba185d4294c2ec94124db6bc25
Musa Ibrahim <musaaj@gmail.com>
2023-01-23 13:26:20 +0100

    Auto Commit

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,7 +14,7 @@ void bubble_sort(int *array, size_t size)
 		return;
 	for (i = 0; i < size; i++)
 	{
-		for (j = i; j < size; j++)
+		for (j = 0; j < size; j++)
 		{
 			if (array[j] > array[i])
 			{
